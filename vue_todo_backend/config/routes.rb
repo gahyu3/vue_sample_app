2ccb106cf497a09b7ab603a1dfbfd35e98a6b46c
ishii hyuga <hyu825@gmail.com>
2024-11-30 19:00:13 +0900

    一覧ページのAPI実装

--- vue_todo_backend/config/routes.rb
+++ vue_todo_backend/config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get '/api/test', to: 'application#test'
+
+  namespace :api do
+    resources :todos, only: [:index]
+  end
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
